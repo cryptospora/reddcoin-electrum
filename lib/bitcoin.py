6a5635a6ab884080f04d23798e3980de719286ed
cryptospora <cryptospora@platinumcrypto.com>
2014-12-23 09:39:49 -0800

    Update bitcoin.py

--- lib/bitcoin.py
+++ lib/bitcoin.py
@@ -235,7 +235,7 @@ def public_key_to_bc_address(public_key):
     return hash_160_to_bc_address(h160)
 
 
-def hash_160_to_bc_address(h160, addrtype=61):
+def hash_160_to_bc_address(h160, addrtype=55):
     vh160 = chr(addrtype) + h160
     h = Hash(vh160)
     addr = vh160 + h[0:4]
@@ -321,13 +321,13 @@ def PrivKeyToSecret(privkey):
     return privkey[9:9+32]
 
 
-def SecretToASecret(secret, compressed=False, addrtype=61):
+def SecretToASecret(secret, compressed=False, addrtype=55):
     vchIn = chr((addrtype+128) & 255) + secret
     if compressed: vchIn += '\01'
     return EncodeBase58Check(vchIn)
 
 
-def ASecretToSecret(key, addrtype=61):
+def ASecretToSecret(key, addrtype=55):
     vch = DecodeBase58Check(key)
     if vch and vch[0] == chr((addrtype+128) & 255):
         return vch[1:]
